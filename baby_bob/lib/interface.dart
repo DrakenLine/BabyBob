1e88431367b53d1d4d90c87a53e363be03d157f5
DrakenLine <alexandra.farina@mmibordeaux.com>
2020-11-25 22:27:15 +0100

    crea nouvelle page

---
+++ baby_bob/lib/interface.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Interface extends StatefulWidget {
+  @override
+  _InterfaceState createState() => _InterfaceState();
+}
+
+class _InterfaceState extends State<Interface> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('coucpu'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Interface')
+          ],
+        ),
+      ),
+    );
+  }
+}
